4f3b819fa46911d85f4f65bac4b7c39756d67171
mosman7 <mohamedosman998@hotmail.com>
2022-11-14 16:43:14 +0000

    ansible

---
+++ provision.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt upgrade
+sudo apt-get install ansible -y
+# sudo apt-get install tree -y
